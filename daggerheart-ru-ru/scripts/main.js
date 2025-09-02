8db4be7c0b0a6f202b2f1b0ddc2039d94018439b
Ujin <37151086+LooseSlives@users.noreply.github.com>
2025-09-02 16:04:41 +0300

    Add files via upload

---
+++ daggerheart-ru-ru/scripts/main.js
@@ -0,0 +1,23 @@
+Hooks.once('babele.init', (babele) => {
+		
+    babele.register({
+      module: 'daggerheart-ru-ru',
+      lang: 'ru',
+      dir: 'translations'
+    });
+		
+		Babele.get().registerConverters({
+			
+			"toAdversariesItems": (original, translation) => {
+				for (i in original){
+					original[i].name = translation[original[i]._id]?.name;
+					original[i].system.description = translation[original[i]._id]?.description;
+				}
+				return;
+			}
+		});
+		
+	};
+	
+});
+
